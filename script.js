3276190c5368aa9219c0ac89687049659bb0d1cd
Eduardo Capdeville <capdevilleeduardo@gmail.com>
2024-03-27 13:58:38 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelectorAll('.navbar ul li a').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            const targetId = this.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+
+            targetElement.scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+});
